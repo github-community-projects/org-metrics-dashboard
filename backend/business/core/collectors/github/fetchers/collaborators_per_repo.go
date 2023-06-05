cb81d2f0ffff3808f1c91ffae053afc32df0a805
Hassan Hawache <hasan-dot@github.com>
2023-06-05 16:43:13 -0700

    Add fetching with pagination and csv formatting

--- backend/business/core/collectors/github/fetchers/collaborators_per_repo.go
+++ backend/business/core/collectors/github/fetchers/collaborators_per_repo.go
@@ -2,9 +2,9 @@ package fetchers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/shurcooL/githubv4"
-	"github.com/who-metrics/business/helpers"
 )
 
 type CollaboratorsPerRepo struct {
@@ -12,38 +12,64 @@ type CollaboratorsPerRepo struct {
 	orgName string
 }
 
+type RepoCollaboratorsInfo struct {
+	Node struct {
+		Name          githubv4.String
+		Collaborators struct {
+			TotalCount githubv4.Int
+		}
+	}
+}
+
 func NewCollaboratorsPerRepo(client *githubv4.Client, orgName string) *CollaboratorsPerRepo {
 	return &CollaboratorsPerRepo{client: client, orgName: orgName}
 }
 
+type collaboratorsQuery struct {
+	Organization struct {
+		Repositories struct {
+			Edges    []RepoCollaboratorsInfo
+			PageInfo struct {
+				EndCursor   githubv4.String
+				HasNextPage bool
+			}
+		} `graphql:"repositories(first: 100, after: $reposCursor)"`
+	} `graphql:"organization(login:$organizationLogin)"`
+}
+
 func (f *CollaboratorsPerRepo) Fetch(ctx context.Context) (string, error) {
-	var q struct {
-		Organization struct {
-			Repositories struct {
-				Edges []struct {
-					Node struct {
-						Name          githubv4.String
-						Collaborators struct {
-							TotalCount githubv4.Int
-						}
-					}
-				}
-			} `graphql:"repositories(first:100)"`
-		} `graphql:"organization(login:$organizationLogin)"`
-	}
 	variables := map[string]interface{}{
 		"organizationLogin": githubv4.String(f.orgName),
+		"reposCursor":       (*githubv4.String)(nil), // Null after argument to get first page.
 	}
 
-	err := f.client.Query(ctx, &q, variables)
+	var q collaboratorsQuery
+
+	var allRepos []RepoCollaboratorsInfo
+
+	for {
+		err := f.client.Query(ctx, &q, variables)
+		if err != nil {
+			return "", err
+		}
+		allRepos = append(allRepos, q.Organization.Repositories.Edges...)
+		if !q.Organization.Repositories.PageInfo.HasNextPage {
+			break
+		}
+		variables["reposCursor"] = githubv4.NewString(q.Organization.Repositories.PageInfo.EndCursor)
+	}
+
+	csvString, err := f.formatCSV(allRepos)
 	if err != nil {
 		return "", err
 	}
+	return csvString, nil
+}
 
-	jsonData, err := helpers.FormatJSON(q)
-
-	if err != nil {
-		return "", err
+func (f *CollaboratorsPerRepo) formatCSV(data []RepoCollaboratorsInfo) (string, error) {
+	csvString := "repo_name,collaborators_count\n"
+	for _, edge := range data {
+		csvString += fmt.Sprintf("%s,%d\n", edge.Node.Name, edge.Node.Collaborators.TotalCount)
 	}
-	return jsonData, nil
+	return csvString, nil
 }
