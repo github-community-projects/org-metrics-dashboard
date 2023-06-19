b760fd8a69c7be14205e23b4242301ca778718aa
Hassan Hawache <hasan-dot@github.com>
2023-06-19 06:29:12 -0700

    update styling

--- who-metrics-ui/src/pages/_document.tsx
+++ who-metrics-ui/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="h-full bg-gray-50">
       <Head />
       <Navbar />
       <body className="h-full">
