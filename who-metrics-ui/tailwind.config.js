a56319ceba686db03cc2e0e70d2498552994cde5
Hassan Hawache <hasan-dot@github.com>
2023-06-19 05:55:23 -0700

    fix tailwind css

--- who-metrics-ui/tailwind.config.js
+++ who-metrics-ui/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}'
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+     // Or if using `src` directory:
+     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {},
