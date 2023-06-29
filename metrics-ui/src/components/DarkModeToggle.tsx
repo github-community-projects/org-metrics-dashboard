509970dc512d930954cd2a06e750ad41e1b0e98a
Hassan Hawache <hasan-dot@github.com>
2023-06-29 11:45:30 -0700

    Add darkmode

---
+++ metrics-ui/src/components/DarkModeToggle.tsx
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+import { useTheme } from "next-themes";
+
+
+const DarkModeToggle = () => {
+    const { systemTheme, theme, setTheme } = useTheme();
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    return (
+        <button
+            onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
+            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
+            Toggle Mode
+        </button>
+    )
+}
+
+export default DarkModeToggle
