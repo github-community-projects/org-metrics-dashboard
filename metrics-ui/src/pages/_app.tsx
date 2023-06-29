a6666cf06d99205559cf6c013ebac054c70f015d
Hassan Hawache <hasan-dot@github.com>
2023-06-29 11:30:33 -0700

    wrap app around theme component

--- metrics-ui/src/pages/_app.tsx
+++ metrics-ui/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "next-themes"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
