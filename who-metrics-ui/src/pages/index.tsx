4759faaf2a4ff8685e6bc0c4e3e92f33c7f661cd
Hassan Hawache <hasan-dot@github.com>
2023-07-01 04:27:47 -0700

    remove unused stuff

--- who-metrics-ui/src/pages/index.tsx
+++ who-metrics-ui/src/pages/index.tsx
@@ -1,30 +1,5 @@
-import { Card, Metric, Text, Flex, Grid, Title, BarList } from '@tremor/react';
-import Chart from '../components/chart';
 import DarkModeToggle from '@/components/DarkModeToggle';
 import OldDashboard from '@/components/OldDashboard';
-
-const website = [
-  { name: '/home', value: 1230 },
-  { name: '/contact', value: 751 },
-  { name: '/gallery', value: 471 },
-  { name: '/august-discount-offer', value: 280 },
-  { name: '/case-studies', value: 78 }
-];
-
-const shop = [
-  { name: '/home', value: 453 },
-  { name: '/imprint', value: 351 },
-  { name: '/shop', value: 271 },
-  { name: '/pricing', value: 191 }
-];
-
-const app = [
-  { name: '/shop', value: 789 },
-  { name: '/product-features', value: 676 },
-  { name: '/about', value: 564 },
-  { name: '/login', value: 234 },
-  { name: '/downloads', value: 191 }
-];
 
 export default function PlaygroundPage() {
   return (
