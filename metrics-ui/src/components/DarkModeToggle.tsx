cbd6bce6ef305d53723380073e4709e90750405d
Hassan Hawache <hasan-dot@github.com>
2023-07-01 04:27:29 -0700

    follow toturial for tremor

--- metrics-ui/src/components/DarkModeToggle.tsx
+++ metrics-ui/src/components/DarkModeToggle.tsx
@@ -1,19 +1,21 @@
 "use client"
 import React from 'react'
 import { useTheme } from "next-themes";
-
+import { Text, ToggleSwitch } from '@primer/react'
 
 const DarkModeToggle = () => {
-    const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === 'system' ? systemTheme : theme;
-
+    const { theme, setTheme } = useTheme();
     return (
-        <button
-            onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
-            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
+        <>
+        <Text id="toggle" fontWeight="bold" fontSize={1}>
             Toggle Mode
-        </button>
-    )
+        </Text>
+        <ToggleSwitch
+            checked={theme == "dark"}
+            aria-labelledby="toggle" 
+            onClick={() => theme == "dark" ? setTheme('light') : setTheme("dark")}    
+        />
+    </>)
 }
 
 export default DarkModeToggle
--- metrics-ui/src/pages/_app.tsx
+++ metrics-ui/src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from "next-themes"
+import {ThemeProvider as PrimerThemeProvider, BaseStyles} from '@primer/react'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <PrimerThemeProvider>
+        <BaseStyles>
+          <Component {...pageProps} />
+        </BaseStyles>
+      </PrimerThemeProvider>
     </ThemeProvider>
   )
 }
