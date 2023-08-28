78ca1fe5461fe03f43407496d7c711855dc7fd96
JoannaaKL <joannaakl@github.com>
2023-08-28 11:42:49 +0000

    Add gh extension to installed tools

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -2,11 +2,17 @@
 
 set -euo pipefail
 
-echo "==> Installing golang tools"
+echo "==> Installing go tools"
 
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/rogpeppe/godef@latest
 
-echo "==> golang tools installation complete!"
+echo "==> go tools installed!"
+
+echo "==> Installing gh extensions"
+
+gh extension install https://github.com/nektos/gh-act
+
+echo "==> gh extensions installed!"
