2faff70fc57a530c1e302961e7f979b19d01ec4d
Ian Candy <ipc103@github.com>
2023-12-05 21:49:21 +0000

    Add React Data Grid
    The React-Data-Grid library includes powerful components with built in
    options for sorting, filtering, etc. This makes the page function more
    similarly to a spreadsheet, which should allow for more power when
    sorting through the data vs. a simple table.
    
    Co-authored-by: Andrew Henry <ajhenry@github.com>
    Co-authored-by: Rachel Stanik <lehcar@github.com>

--- who-metrics-ui/src/components/RepositoriesTable.tsx
+++ who-metrics-ui/src/components/RepositoriesTable.tsx
@@ -1,26 +1,11 @@
 import { InfoIcon } from "@primer/octicons-react";
 import { Tooltip } from "@primer/react";
-import {
-  Card,
-  Select,
-  Flex,
-  SelectItem,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeaderCell,
-  TableRow,
-  Title,
-} from "@tremor/react";
-import { useState } from "react";
+import { Card, Flex, Title, Text } from "@tremor/react";
+import DataGrid from "react-data-grid";
+
 import Data from "../data/data.json";
-import { RepositorySelector } from "./RepositorySelector";
 
 const repos = Object.values(Data["repositories"]);
-const licenses = repos
-  .map((repo) => repo.licenseName)
-  .filter((licenseName, index, array) => array.indexOf(licenseName) === index);
 type Repo = (typeof repos)[0];
 
 const Labels: Record<string, keyof Repo> = {
@@ -36,14 +21,17 @@ const Labels: Record<string, keyof Repo> = {
   Forks: "forksCount",
 } as const;
 
-const headers = Object.keys(Labels);
+const DataGridColumns = Object.keys(Labels).map((label) => {
+  return {
+    key: Labels[label],
+    name: label,
+  };
+});
 
 const RepositoriesTable = () => {
-  const [selectedLicense, setSelectedStatus] = useState("all");
-  const [selectedNames, setSelectedNames] = useState<string[]>([]);
-  const isRepoSelected = (repo: Repo) =>
-    (repo.licenseName === selectedLicense || selectedLicense === "all") &&
-    (selectedNames.includes(repo.repositoryName) || selectedNames.length === 0);
+  const subTitle = () => {
+    return `${repos.length} total repositories`;
+  };
   return (
     <Card className="mt-6">
       <>
@@ -57,61 +45,18 @@ const RepositoriesTable = () => {
             <Tooltip aria-label="All of the repositories in this organization">
               <InfoIcon size={24} />
             </Tooltip>
+            <Text>{subTitle()}</Text>
           </Flex>
         </div>
-        <div className="flex space-x-2">
-          <RepositorySelector
-            selectedNames={selectedNames}
-            setSelectedNames={setSelectedNames}
-            repositoryNames={repos.map((repo) => repo.repositoryName)}
-          />
-          <Select
-            className="max-w-full sm:max-w-xs"
-            defaultValue="all"
-            onValueChange={setSelectedStatus}
-          >
-            {licenses.map((license) => (
-              <SelectItem key={license} value={license}>
-                {license === "" ? "None" : license}
-              </SelectItem>
-            ))}
-          </Select>
-        </div>
-        <Table className="mt-6">
-          <TableHead>
-            <TableRow>
-              {headers.map((label, index) => (
-                <TableHeaderCell
-                  key={label}
-                  className={index === 0 ? "" : "text-right"}
-                >
-                  {label}
-                </TableHeaderCell>
-              ))}
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {repos
-              .filter((repo) => isRepoSelected(repo))
-              .map((repo) => (
-                <TableRow key={repo.repositoryName}>
-                  {headers.map((header, index) => {
-                    const property = Labels[header];
-                    const value = repo[property];
-                    return (
-                      <TableCell
-                        key={value}
-                        className={index === 0 ? "" : "text-right"}
-                      >
-                        {value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
+        <DataGrid
+          columns={DataGridColumns}
+          rows={repos}
+          rowKeyGetter={(repo) => repo.repoName}
+          defaultColumnOptions={{
+            sortable: true,
+            resizable: true,
+          }}
+        />
       </>
     </Card>
   );
